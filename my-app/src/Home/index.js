433ed455b2c3acf82d34d5c1f6456dbd80eba7fd
krinagajera19 <gajera.krina04@gmail.com>
2024-08-05 16:37:08 +0530

    Add Home component and commented out ApiCallCom

--- my-app/src/Home/index.js
+++ my-app/src/Home/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ApiCallCom from "../Components/useEffectApi";
+// import React from "react";
+// import ApiCallCom from "../Components/useEffectApi";
 
-const Home = () => {
-  return (
-    <>
-      <ApiCallCom />
-    </>
-  );
-};
+// const Home = () => {
+//   return (
+//     <>
+//       <ApiCallCom />
+//     </>
+//   );
+// };
 
-export default Home;
+// export default Home;
