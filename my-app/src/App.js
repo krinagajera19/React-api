ed96f841d17e874fda608de987557b41073d5d36
krinagajera19 <gajera.krina04@gmail.com>
2024-08-05 16:53:50 +0530

    initial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,34 @@
-// import React from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./Home";
-// import Demo from "./Components/Demo";
-// import Redirect from "./Components/Redirect";
-// import Ref from "./Components/UseRef";
-// import UseReducercomponent from "./Components/UseReducer";
-// import Debug from "./Components/UseDebug";
-// import MyComponent from "./Uselayout";
-// import AuthProvider from "./Components/ContextApi";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Demo from "./Components/Demo";
+import Redirect from "./Components/Redirect";
+import Ref from "./Components/UseRef";
+import UseReducercomponent from "./Components/UseReducer";
+import Debug from "./Components/UseDebug";
+import MyComponent from "./Uselayout";
+import AuthProvider from "./Components/ContextApi";
 
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/demo" element={<Demo />} />
-//           <Route path="/redirect" element={<Redirect />} />
-//           <Route path="/ref" element={<Ref />} />
-//           <Route path="/debug" element={<Debug />} />
-//           <Route
-//             path="/useReducercomponent"
-//             element={<UseReducercomponent />}
-//           />
-//           <Route path="/MyComponent" element={<MyComponent />} />
-//           <Route path="/Authprovider" element={<AuthProvider />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </AuthProvider>
-//   );
-// }
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/redirect" element={<Redirect />} />
+          <Route path="/ref" element={<Ref />} />
+          <Route path="/debug" element={<Debug />} />
+          <Routes
+            path="/useReducercomponent"
+            element={<UseReducercomponent />}
+          />
+          <Route path="/MyComponent" element={<MyComponent />} />
+          <Route path="/Authprovider" element={<AuthProvider />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
 
-// export default App;
+export default App;
